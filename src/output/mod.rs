a1c327600006eb027efaa3e2063cb8556cf6c720
chenquan <chenquan.dev@gmail.com>
2025-03-08 10:52:38 +0800

    feat: add json_to_arrow and arrow_to_json

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -30,7 +30,7 @@ pub trait Output: Send + Sync {
 
 /// 输出配置
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "lowercase")]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum OutputConfig {
     File(file::FileOutputConfig),
     Http(http::HttpOutputConfig),
